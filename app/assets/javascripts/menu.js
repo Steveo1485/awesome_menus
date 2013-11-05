4ed21b9ee4332dfbec9d8bff6b6f0fcd09bd5d6c
Apprentice <apprentice@dbc-19.local>
2013-11-04 19:14:48 -0800

    Add AJAX with Rails

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,23 +1,31 @@
 var Menu = {
   init : function(){
-    $('.form-create-menu').on('submit', this.getMenuName)
+    $('.form-create-menu').on("ajax:success", this.appendMenu);
+    // $('.form-create-menu').on('submit', this.getMenuName)
   },
 
-  getMenuName : function(e){
-    e.preventDefault();
-    var $menuForm = $(this)
-    $.ajax({
-      url: $menuForm.prop("action"),
-      type: $menuForm.prop("method"),
-      data: $menuForm.serialize()
-    }).done(function(formCreateMenuData){
-      Menu.appendMenu(formCreateMenuData.new_menu_name);
-    });
-  },
+  // getMenuName : function(e){
+  //   e.preventDefault();
+  //   var $menuForm = $(this)
+  //   $.ajax({
+  //     url: $menuForm.prop("action"),
+  //     type: $menuForm.prop("method"),
+  //     data: $menuForm.serialize()
+  //   }).done(function(formCreateMenuData){
+  //     Menu.appendMenu(formCreateMenuData.new_menu_name);
+  //   });
+  // },
 
-  appendMenu : function(dataToAppend){
-    $('.menu-list').append(dataToAppend);
-  }
+  //Rails AJAX Append Function
+    appendMenu: function(e, dataToAppend){
+      $('.menu-list').append(dataToAppend.new_menu_name)
+    }
+
+
+  //jQuery AJAX Append Function
+  // appendMenu : function(dataToAppend){
+  //   $('.menu-list').append(dataToAppend);
+  // }
 }
 
 
