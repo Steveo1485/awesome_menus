e4babf94142a228266cc197b7452ce704d25d9d6
Steven Nugent <stevennugentdev@gmail.com>
2013-11-05 08:57:11 -0800

    Add menu works with Rails AJAX, text field clears on submit, main page styled

--- spec/features/create_menu_spec.rb
+++ spec/features/create_menu_spec.rb
@@ -5,7 +5,7 @@ feature 'Menu' do
   scenario "Should appear on index once created" do
     visit(root_path)
     fill_in("menu_name", with: "Breakfast")
-    click_button("submit")
+    click_button("Add Menu")
     expect(page).to have_content("Breakfast")
   end
 end
